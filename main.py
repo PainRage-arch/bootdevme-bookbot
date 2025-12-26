dbfa587e39edc28e81a53ef982bfc65ece2ffa0c
Jessie P. <shiadalu@gmail.com>
2025-12-26 06:47:09 -0600

    created text exctraction

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def main():
+    
+    with open("books/frankenstein.txt") as file:
+        content = file.read()
+        
+        print(content)
+        return content
+if __name__ == "__main__":
+    main()
