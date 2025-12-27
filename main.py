4d03903c01bd0bb0a1e4e8d7f8e52c14bf47ccc6
Jessie P. <shiadalu@gmail.com>
2025-12-26 19:45:55 -0600

    working on returning counted characters from a text file.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from stats import get_num_words
+from stats import get_num_characters
 def main():
     
     with open("books/frankenstein.txt") as file:
         content = file.read()
     
-    num_words = get_num_words(content)    
+    num_words = get_num_words(content)
+    print(f"Found {num_characters} total characters")    
     print(f"Found {num_words} total words")
 
 if __name__ == "__main__":
--- stats.py
+++ stats.py
@@ -1,3 +1,14 @@
 def get_num_words(content):
     words = content.split()
     return len(words)
+
+def get_num_characters(content):
+    character_count = {}
+    words = content.split()
+    character = words.split()
+    for char in character:
+        if char.lower() not in character_count:
+            character_count[char.lower()] = 1
+        else:
+            character_count[char] += 1
+    return character_count
