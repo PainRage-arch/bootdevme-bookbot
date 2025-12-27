16eb934c86227d31a2bce87686706b6969df27a2
Jessie P. <shiadalu@gmail.com>
2025-12-26 19:22:16 -0600

    clean import stats

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
+from stats import get_num_words
 def main():
     
     with open("books/frankenstein.txt") as file:
         content = file.read()
-        
-        print(content)
-        return content
+    
+    num_words = get_num_words(content)    
+    print(f"Found {num_words} total words")
+
 if __name__ == "__main__":
     main()
