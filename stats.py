ec8d49153f91ee4e0766fe20f60de650d6b8101d
Jessie P. <shiadalu@gmail.com>
2025-12-26 19:55:24 -0600

    update index

--- stats.py
+++ stats.py
@@ -8,6 +8,6 @@ def get_num_characters(content):
         if char.lower() not in character_count:
             character_count[char.lower()] = 1
         else:
-            character_count[char] += 1
+            character_count[char.lower()] += 1
     print(character_count)
     return character_count
